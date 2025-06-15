5b030856a448fb143e35bf93ab824d0390684b92
AkshayRsenal <akshayraul07@gmail.com>
2025-06-15 18:25:26 +0200

    added additional Unit Tests for Gutter usecases

--- src/main/java/com/group/activities/bowling/entity/BowlingGame.java
+++ src/main/java/com/group/activities/bowling/entity/BowlingGame.java
@@ -10,20 +10,25 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
 public class BowlingGame {
-    
+
     private List<Integer> rolls = new ArrayList<>(21); // Maximum of 21 rolls in a game
     private int initialRoll = 0;
-    private static final String INVALIDPINS_STRING = "Pins that fall out must be between 0 and 10."; 
+    private int score = 0;
+
+    private static final String INVALIDPINS_STRING = "Pins that fall out must be between 0 and 10.";
 
     public void roll(int pinsOut) {
         if (pinsOut < 0 || pinsOut > 10) {
             throw new IllegalArgumentException(INVALIDPINS_STRING);
         }
-
         rolls.add(pinsOut);
+        score += pinsOut; // Update the score with the number of pins knocked down
+    }
+
+    public int getCurrentScore() {
+        return score;
     }
 
 }
--- src/test/java/com/group/activities/bowling/unit/entity/BowlingGameTest.java
+++ src/test/java/com/group/activities/bowling/unit/entity/BowlingGameTest.java
@@ -3,6 +3,7 @@ package com.group.activities.bowling.unit.entity;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.group.activities.bowling.entity.BowlingGame;
@@ -14,9 +15,8 @@ public class BowlingGameTest {
     @BeforeEach
     public void setUp() {
         // Initialize a new BowlingGame instance before each test
-         bowlingGame = new BowlingGame();
+        bowlingGame = new BowlingGame();
     }
-
 
     @Test
     public void whenRollsAdded_thenAssertContains() {
@@ -25,17 +25,37 @@ public class BowlingGameTest {
         bowlingGame.roll(4);
         bowlingGame.roll(5);
         bowlingGame.roll(6);
-        System.out.println("Rolls: " + bowlingGame.getRolls());
         assertThat(bowlingGame.getRolls())
-        .isNotNull()
-        .isNotEmpty()
-        .contains( 5, 6);
+                .isNotNull()
+                .isNotEmpty()
+                .contains(5, 6);
     }
 
     @Test
+    @DisplayName("Nach Gutter-Roll assert, ob Punkzahl ist 0")
     public void whenGutterRoll_thenAssertScoreZero() {
         bowlingGame.roll(0);
+        assertThat(bowlingGame.getCurrentScore()).isEqualTo(0);
     }
 
-    
+    @Test
+    @DisplayName("Nach alle Gutter-Rolls in Spiel assert, ob Gesamtpunkzahl ist 0")
+    void whenGutterGame_thenAssertTotalScoreZero() {
+        for (int i = 0; i < 20; i++) {
+            whenGutterRoll_thenAssertScoreZero();
+        }
+        assertThat(bowlingGame.getCurrentScore()).isEqualTo(0);
+        assertThat(bowlingGame.getRolls()).contains(0, 0, 0);
+    }
+
+    @Test
+    @DisplayName("Nach alle //TODO Gutter-Rolls in Spiel assert, ob Gesamtpunkzahl ist 80")
+    void whenAllFours_thenAssertTotalScoreIsEighty() {
+        for (int i = 0; i < 20; i++) {
+            bowlingGame.roll(4);
+        }
+        assertThat(bowlingGame.getCurrentScore()).isEqualTo(80);
+        assertThat(bowlingGame.getRolls()).contains(4, 4, 4);
+    }
+
 }
