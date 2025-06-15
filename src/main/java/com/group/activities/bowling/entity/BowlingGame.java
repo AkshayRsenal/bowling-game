123502269f9db5e5c61b40cb92f726f65f1139c9
AkshayRsenal <akshayraul07@gmail.com>
2025-06-15 16:50:02 +0200

    test and method for rolls in Game

--- src/main/java/com/group/activities/bowling/entity/BowlingGame.java
+++ src/main/java/com/group/activities/bowling/entity/BowlingGame.java
@@ -1,5 +1,29 @@
 package com.group.activities.bowling.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class BowlingGame {
     
+    private List<Integer> rolls = new ArrayList<>(21); // Maximum of 21 rolls in a game
+    private int initialRoll = 0;
+    private static final String INVALIDPINS_STRING = "Pins that fall out must be between 0 and 10."; 
+
+    public void roll(int pinsOut) {
+        if (pinsOut < 0 || pinsOut > 10) {
+            throw new IllegalArgumentException(INVALIDPINS_STRING);
+        }
+
+        rolls.add(pinsOut);
+    }
+
 }
--- src/test/java/com/group/activities/bowling/unit/entity/BowlingGameTest.java
+++ src/test/java/com/group/activities/bowling/unit/entity/BowlingGameTest.java
@@ -1,5 +1,7 @@
 package com.group.activities.bowling.unit.entity;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -17,17 +19,23 @@ public class BowlingGameTest {
 
 
     @Test
-    public void whenRollsAdded_thenReturnsTrue() {
-       
+    public void whenRollsAdded_thenAssertContains() {
+        // Example test case to check if rolls can be added
+        bowlingGame.roll(3);
+        bowlingGame.roll(4);
+        bowlingGame.roll(5);
+        bowlingGame.roll(6);
+        System.out.println("Rolls: " + bowlingGame.getRolls());
+        assertThat(bowlingGame.getRolls())
+        .isNotNull()
+        .isNotEmpty()
+        .contains( 5, 6);
     }
 
     @Test
-    public void checkroll() {
-
+    public void whenGutterRoll_thenAssertScoreZero() {
+        bowlingGame.roll(0);
     }
 
-    @Test
-    void testGetTotalScore() {
-
-    }
+    
 }
